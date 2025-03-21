c2f0c34dcd76881e1bdf691b429bdac6f353a200
Cedric Arts <152818750+cedricarts@users.noreply.github.com>
2025-03-21 21:19:39 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const button = document.querySelector(".learn-more");
+    button.addEventListener("click", function() {
+        alert("More details coming soon!");
+    });
+});
